b57634d3386d2306bc26ed45537aa5a603555cd5
Smokey D <smokeysmokeyd@gmail.com>
2013-05-13 22:16:01 -0400

    Postcard background image now loads at page load

--- js/nobo.js
+++ js/nobo.js
@@ -234,7 +234,7 @@ var page =
 			popup_msg("", false, true);
 
 		for (var i=0; i<menu_opts.length; i++)
-			$(menu_opts[i]).style.display = ( el == null ? "none" : ( (menu_opts[i] == id) ? ($(id).style.display=="block" ? "none" : "block") : "none" ));
+			$(menu_opts[i]).style.marginTop = ( el == null ? "99999px" : ( (menu_opts[i] == id) ? ($(id).style.marginTop=="40px" ? "99999px" : "40px") : "99999px" ));
 			
 
 		// must blue focus on any text fields if they are not displayed so keyboarddefaults works currectly
